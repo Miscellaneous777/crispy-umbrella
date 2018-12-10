45736b1e2581a9618e5dc4913067f05ce2529ff5
Miscellaneous777 <43757346+Miscellaneous777@users.noreply.github.com>
2018-12-10 17:07:56 +0000

    All my OG Files
    yOU kNoW WhRaT ThiS iS

---
+++ js/javaS.js
@@ -0,0 +1,21 @@
+function init(){
+	//$index.ready(function(){console.log("ready!")});
+	$( document ).ready(function(){console.log( "ready!" );});
+	//
+	
+	if (typeof(Storage) != undefined){
+		
+		
+	}else{
+		
+		
+	}
+
+
+}
+
+
+window.onload = function(){
+
+	init();
+}
